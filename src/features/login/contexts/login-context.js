fe3ce2a5d3fcb6c75d87126ec5d4c3651eb92832
Lima2006 <gabrielchagaslima2015@gmail.com>
2024-11-28 22:24:57 -0300

    refactor: use useLogin hook at login screen

--- src/features/login/contexts/login-context.js
+++ src/features/login/contexts/login-context.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { AUTH_TOKEN_KEY } from "../../core/hooks/use-login";
 
 export const LoginContext = React.createContext();
 
 export const LoginContextProvider = ({ children }) => {
-  const [user, setUser] = React.useState(localStorage.getItem("token") || null);
+  const [user, setUser] = React.useState(
+    localStorage.getItem(AUTH_TOKEN_KEY) || null
+  );
 
   return (
     <LoginContext.Provider value={{ user, setUser }}>
--- src/lib/api.js
+++ src/lib/api.js
@@ -1,5 +1,7 @@
 import axios from "axios";
+import { AUTH_TOKEN_KEY } from "../features/core/hooks/use-login";
 
 export const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
+  headers: { Authorization: `Bearer ${localStorage.getItem(AUTH_TOKEN_KEY)}` },
 });
