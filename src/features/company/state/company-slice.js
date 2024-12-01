0c458139854c19d478925a7383cb5ffcb6dbb4e3
Lima2006 <gabrielchagaslima2015@gmail.com>
2024-12-01 13:15:12 -0300

    feat: company state reducer

---
+++ src/features/company/state/company-slice.js
@@ -0,0 +1,70 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { message } from "antd";
+
+const initialState = {
+  companies: [],
+  totalCompanies: 0,
+  isLoading: false,
+  error: null,
+};
+
+// Like the React Reducer
+const companiesSlice = createSlice({
+  name: "companies",
+  initialState,
+  reducers: {
+    fetchCompaniesRequest: (state) => {
+      state.isLoading = true;
+      state.error = null;
+    },
+    fetchCompaniesSuccess: (state, action) => {
+      state.isLoading = false;
+      state.companies = action.payload.companies;
+      state.totalCompanies = action.payload.total;
+    },
+    fetchCompaniesFailure: (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload;
+    },
+    addCompanyRequest: (state) => {
+      state.isLoading = true;
+    },
+    addCompanySuccess: (state, action) => {
+      state.isLoading = false;
+      state.companies.push(action.payload);
+    },
+    addCompanyFailure: (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload;
+    },
+    deleteCompanyRequest: (state) => {
+      state.isLoading = true;
+    },
+    deleteCompanySuccess: (state, action) => {
+      state.isLoading = false;
+      state.companies = state.companies.filter(
+        (company) => company._id !== action.payload
+      );
+      message.success("Contrato excluído com sucesso!");
+    },
+    deleteCompanyFailure: (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload;
+      message.error("Erro ao excluir contrato.");
+    },
+  },
+});
+
+export const {
+  fetchCompaniesRequest,
+  fetchCompaniesSuccess,
+  fetchCompaniesFailure,
+  addCompanyRequest,
+  addCompanySuccess,
+  addCompanyFailure,
+  deleteCompanyRequest,
+  deleteCompanySuccess,
+  deleteCompanyFailure,
+} = companiesSlice.actions;
+
+export default companiesSlice.reducer;
