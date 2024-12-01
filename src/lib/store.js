d0e20c032661efe002f1ae79de25169a28d923f8
Lima2006 <gabrielchagaslima2015@gmail.com>
2024-12-01 13:16:04 -0300

    feat: use company saga middleware

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,6 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "redux-saga";
+import companySaga from "../features/company/state/company-sagas";
 import contractSaga from "../features/contracts/state/contract-sagas";
+import companyReducer from "../features/company/state/company-slice";
 import contractReducer from "../features/contracts/state/contract-slice";
 
 const sagaMiddleware = createSagaMiddleware();
@@ -8,9 +10,11 @@ const sagaMiddleware = createSagaMiddleware();
 export const store = configureStore({
   reducer: {
     contracts: contractReducer,
+    companies: companyReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({ thunk: false }).concat(sagaMiddleware),
 });
 
 sagaMiddleware.run(contractSaga);
+sagaMiddleware.run(companySaga);
